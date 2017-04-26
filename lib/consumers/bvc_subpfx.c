422425df556bbd47b6d4bdb1557be868a8f496f8
Alistair King <alistair@caida.org>
2017-04-26 11:46:04 -0700

    Add file output to sub-pfx consumer

--- lib/consumers/bvc_subpfx.c
+++ lib/consumers/bvc_subpfx.c
@@ -23,6 +23,7 @@
 
 #include <assert.h>
 #include <ctype.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -30,6 +31,8 @@
 #include "bgpstream_utils_patricia.h"
 #include "khash.h"
 #include "utils.h"
+#include "wandio.h"
+#include "wandio_utils.h"
 #include "bvc_subpfx.h"
 
 #define NAME "subpfx"
@@ -43,6 +46,11 @@
 
 #define CUR_SUBPFXS (STATE->subpfxs[STATE->current_subpfxs_idx])
 #define PREV_SUBPFXS (STATE->subpfxs[(STATE->current_subpfxs_idx + 1) % 2])
+
+#define DEFAULT_OUTPUT_DIR "./"
+#define DEFAULT_COMPRESS_LEVEL 6
+#define OUTPUT_FILE_FORMAT "%s/" NAME ".%" PRIu32 ".events.gz"
+#define BUFFER_LEN 4096
 
 /* Maps sub-prefixes to super prefixes */
 KHASH_INIT(pfx2pfx, bgpstream_pfx_storage_t, bgpstream_pfx_storage_t, 1,
@@ -68,6 +76,9 @@ static bvc_t bvc_subpfx = { //
 /* our 'instance' */
 typedef struct bvc_subpfx_state {
 
+  // options:
+  char *outdir;
+
   // Patricia tree used to find sub-prefixes in the current view
   bgpstream_patricia_tree_t *pt;
 
@@ -81,12 +92,21 @@ typedef struct bvc_subpfx_state {
   // ((current_subpfxs_idx+1)%2) is the map for the previous view
   int current_subpfxs_idx;
 
+  // current output file name
+  char outfile_name[BUFFER_LEN];
+
+  // current output file handle
+  iow_t *outfile;
+
 } bvc_subpfx_state_t;
 
 /** Print usage information to stderr */
 static void usage(bvc_t *consumer)
 {
-  fprintf(stderr, "consumer usage: %s\n", consumer->name);
+  fprintf(stderr, "consumer usage: %s\n"
+          "       -o <output-dir>      output directory (default: %s)\n",
+          consumer->name,
+          DEFAULT_OUTPUT_DIR);
 }
 
 /** Parse the arguments given to the consumer */
@@ -100,8 +120,13 @@ static int parse_args(bvc_t *consumer, int argc, char **argv)
   optind = 1;
 
   /* remember the argv strings DO NOT belong to us */
-  while ((opt = getopt(argc, argv, ":?")) >= 0) {
+  while ((opt = getopt(argc, argv, ":o:?")) >= 0) {
     switch (opt) {
+    case 'o':
+      STATE->outdir = strdup(optarg);
+      assert(STATE->outdir != NULL);
+      break;
+
     case '?':
     case ':':
     default:
@@ -146,15 +171,83 @@ static void find_subpfxs(bgpstream_patricia_tree_t *pt,
   kh_val(CUR_SUBPFXS, k) = tmp_super_pfx;
 }
 
-static int dump_subpfx(bvc_t *consumer, bgpview_t *view,
+static int dump_as_paths(bvc_t *consumer, bgpview_t *view, bgpview_iter_t *it,
+                         bgpstream_pfx_t *pfx)
+{
+  bgpstream_as_path_t *path;
+  char path_str[BUFFER_LEN];
+
+  // seek the iterator to this prefix (it is guaranteed to be in the view)
+  if (bgpview_iter_seek_pfx(it, pfx, BGPVIEW_FIELD_ACTIVE) == 0) {
+    fprintf(stderr, "ERROR: failed to find prefix in view\n");
+    return -1;
+  }
+
+  int first_path = 1;
+  // spin through the peers for this prefix and dump out their AS paths
+  for (bgpview_iter_pfx_first_peer(it, BGPVIEW_FIELD_ACTIVE); //
+       bgpview_iter_pfx_has_more_peer(it); //
+       bgpview_iter_pfx_next_peer(it)) {
+    if (first_path == 0) {
+      wandio_printf(STATE->outfile, ":");
+    }
+
+    // write this AS path out
+    // TODO: optimize this to not construct an AS path each time
+    path = bgpview_iter_pfx_peer_get_as_path(it);
+    assert(path != NULL);
+    bgpstream_as_path_snprintf(path_str, BUFFER_LEN, path);
+    bgpstream_as_path_destroy(path);
+
+    wandio_printf(STATE->outfile, "%s", path_str);
+
+    first_path = 0;
+  }
+
+  return 0;
+}
+
+static int dump_subpfx(bvc_t *consumer, bgpview_t *view, bgpview_iter_t *it,
                        bgpstream_pfx_storage_t *pfx,
                        bgpstream_pfx_storage_t *super_pfx,
                        int diff_type)
 {
+  /* output file format: */
+  /* TIME|SUPER_PFX|SUB_PFX|NEW/FINISHED|SUPER_PFX_PATHS|SUB_PFX_PATHS */
+  /* NB: in FINISHED events, the PATHS fields will be empty */
+  /* since AS path strings can contain commas, the AS paths with be
+     colon-separated, e.g.:
+     AS1 AS2 {AS3,AS4}:AS1 AS2 AS5
+  */
+
+  char pfx_str[INET6_ADDRSTRLEN+3];
+  bgpstream_pfx_snprintf(pfx_str, INET6_ADDRSTRLEN + 3, (bgpstream_pfx_t*)pfx);
+  char super_pfx_str[INET6_ADDRSTRLEN+3];
+  bgpstream_pfx_snprintf(super_pfx_str, INET6_ADDRSTRLEN + 3,
+                         (bgpstream_pfx_t*)super_pfx);
+
+  wandio_printf(STATE->outfile,
+                "%"PRIu32"|%s|%s|%s|",
+                bgpview_get_time(view),
+                super_pfx_str,
+                pfx_str,
+                diff_type_strs[diff_type]);
+
+  if (diff_type == NEW) {
+    // dump the AS paths
+    dump_as_paths(consumer, view, it, (bgpstream_pfx_t*)super_pfx);
+    wandio_printf(STATE->outfile, "|");
+    dump_as_paths(consumer, view, it, (bgpstream_pfx_t*)pfx);
+    wandio_printf(STATE->outfile, "\n");
+  } else {
+    // just finish the record with nulls
+    wandio_printf(STATE->outfile, "|\n");
+  }
+
   return 0;
 }
 
-static int subpfxs_diff(bvc_t *consumer, bgpview_t *view,
+static int subpfxs_diff(bvc_t *consumer, bgpview_t *view, bgpview_iter_t *it,
                         khash_t(pfx2pfx) *a, khash_t(pfx2pfx) *b, int diff_type)
 {
   khiter_t k;
@@ -169,7 +262,7 @@ static int subpfxs_diff(bvc_t *consumer, bgpview_t *view,
     }
 
     // this is a new/finished sub-pfx!
-    if (dump_subpfx(consumer, view, pfx, &kh_val(a, k), diff_type) != 0) {
+    if (dump_subpfx(consumer, view, it, pfx, &kh_val(a, k), diff_type) != 0) {
       return -1;
     }
   }
@@ -230,6 +323,9 @@ void bvc_subpfx_destroy(bvc_t *consumer)
     return;
   }
 
+  free(STATE->outdir);
+  STATE->outdir = NULL;
+
   bgpstream_patricia_tree_destroy(state->pt);
   state->pt = NULL;
   bgpstream_patricia_tree_result_set_destroy(&state->pt_res);
@@ -248,6 +344,17 @@ void bvc_subpfx_destroy(bvc_t *consumer)
 int bvc_subpfx_process_view(bvc_t *consumer, bgpview_t *view)
 {
   bgpview_iter_t *it = NULL;
+
+  // open the output file
+  snprintf(STATE->outfile_name, BUFFER_LEN, OUTPUT_FILE_FORMAT,
+           STATE->outdir, bgpview_get_time(view));
+  if ((STATE->outfile = wandio_wcreate(STATE->outfile_name,
+                                       wandio_detect_compression_type(STATE->outfile_name),
+                                       DEFAULT_COMPRESS_LEVEL, O_CREAT)) == NULL) {
+    fprintf(stderr, "ERROR: Could not open %s for writing\n",
+            STATE->outfile_name);
+    goto err;
+  }
 
   /* create a new iterator */
   if ((it = bgpview_iter_create(view)) == NULL) {
@@ -286,12 +393,13 @@ int bvc_subpfx_process_view(bvc_t *consumer, bgpview_t *view)
 
   // now that we have a table of sub-prefixes, find out which are new
   // (i.e., which are in this view but not in the previous one)
-  if (subpfxs_diff(consumer, view, CUR_SUBPFXS, PREV_SUBPFXS, NEW) != 0) {
+  if (subpfxs_diff(consumer, view, it, CUR_SUBPFXS, PREV_SUBPFXS, NEW) != 0) {
     fprintf(stderr, "ERROR: Failed to find NEW sub prefixes\n");
     goto err;
   }
   // and then do the complement to find finished sub-pfxs
-  if (subpfxs_diff(consumer, view, PREV_SUBPFXS, CUR_SUBPFXS, FINISHED) != 0) {
+  if (subpfxs_diff(consumer, view, it,
+                   PREV_SUBPFXS, CUR_SUBPFXS, FINISHED) != 0) {
     fprintf(stderr, "ERROR: Failed to find NEW sub prefixes\n");
     goto err;
   }
@@ -306,9 +414,26 @@ int bvc_subpfx_process_view(bvc_t *consumer, bgpview_t *view)
   /* empty the patricia tree */
   bgpstream_patricia_tree_clear(STATE->pt);
 
+  /* close the output file */
+  wandio_wdestroy(STATE->outfile);
+  STATE->outfile = NULL;
+
+  /* generate the .done file */
+  snprintf(STATE->outfile_name, BUFFER_LEN, OUTPUT_FILE_FORMAT ".done",
+           STATE->outdir, bgpview_get_time(view));
+  if ((STATE->outfile = wandio_wcreate(STATE->outfile_name,
+                                       wandio_detect_compression_type(STATE->outfile_name),
+                                       DEFAULT_COMPRESS_LEVEL, O_CREAT)) == NULL) {
+    fprintf(stderr, "ERROR: Could not open %s for writing\n", STATE->outfile_name);
+    return -1;
+  }
+  wandio_wdestroy(STATE->outfile);
+  STATE->outfile = NULL;
+
   return 0;
 
  err:
   bgpview_iter_destroy(it);
+  wandio_wdestroy(STATE->outfile);
   return -1;
 }
